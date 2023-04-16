00918cfc976440ff387787dbeaecf803389fea9f
amexstedux <100746766+amexstedux@users.noreply.github.com>
2023-04-16 15:06:27 -0500

    contact form
    almost set up

--- index.js
+++ index.js
@@ -14,3 +14,19 @@
     });
   }, false);
 })();
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "RBEdrilling@gmail.com",
+        Password : "password",
+        To : 'RBEdrilling@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New Contact Form Submission",
+        Body : "Name: "+ document.getElementById("name").value
+            +"<br> Email: " + document.getElementById("email").value
+            +"<br> Message: " + document.getElementById("message").value
+    }).then(
+      message => alert("Message Sent Successfully")
+    );
+}
